220fc4ebba61672c8b7426ab62e867094afa259a
Nikolay <kombarov55@gmail.com>
2018-04-07 23:22:55 +0300

    added mock data for requests

--- queries.sh
+++ queries.sh
@@ -1,16 +1,21 @@
 #https://api.producthunt.com/v1
 #access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff
 
+###########
 #trending-topics
 #https://api.producthunt.com/v1/topics?search[trending]=true&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff
 # jq '.topics[0] | {name: .name, description: .description, followers_count: .followers_count, image: .image}'
 
+#весь:
 curl "https://api.producthunt.com/v1/topics?search\[trending\]=true&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff" | json_pp
 
+#названия:
+curl "https://api.producthunt.com/v1/topics?search\[trending\]=true&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff" | jq '.topics[].name '
 
+##############
 #by-topic
 #jq '.posts[0] | {name: .name, thumbnail: .thumbnail.image_url, description: .tagline}'
 #https://api.producthunt.com/v1/posts/all?search[Tech]=1&page=1&per_page=3&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff
-curl "https://api.producthunt.com/v1/posts/all?search\[Tech\]=1&page=1&per_page=3&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff" | json_pp
+curl "https://api.producthunt.com/v1/posts/all?search\[Touch Bar Apps\]=1&page=1&per_page=3&access_token=591f99547f569b05ba7d8777e2e0824eea16c440292cce1f8dfb3952cc9937ff" | json_pp
 
 
